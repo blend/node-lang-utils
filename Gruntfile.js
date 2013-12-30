cd28295a6492719d050e54466e951d2ae2689fb5
Eugene Marinelli <eugeneiiim@gmail.com>
2013-12-29 21:41:59 -0500

    gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          grep: grunt.option('grep')
+        },
+        src: ['test/*.js']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('default', ['mochaTest:test']);
+};
